8f6c30f38a57ac22a5464d9411e2d6d8c7f1ce3e
Proless <proless8@gmail.com>
2020-07-20 17:49:56 +0200

    set the approved date of a game to null if the response was 1 or 0

--- NexusModsNET/Internals/Converters/GameApprovedDateConverter.cs
+++ NexusModsNET/Internals/Converters/GameApprovedDateConverter.cs
@@ -8,6 +8,13 @@ namespace NexusModsNET.Internals.Converters
 	{
 		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
 		{
+			if (reader.Value is long date)
+			{
+				if (date < 2)
+				{
+					return null;
+				}
+			}
 			return base.ReadJson(reader, objectType, existingValue, serializer);
 		}
 
