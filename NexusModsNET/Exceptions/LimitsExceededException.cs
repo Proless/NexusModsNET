93ca0b7ad309d6f6d19e4a580c32bdd8806e9941
Proless <proless8@gmail.com>
2020-08-29 15:20:06 +0200

    A custom exception class which represents the API Limits exceeded exception.

---
+++ NexusModsNET/Exceptions/LimitsExceededException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+
+namespace NexusModsNET.Exceptions
+{
+	/// <summary>
+	/// This Exception is thrown whenever a Limit has been exceeded
+	/// </summary>
+	[Serializable]
+	public class LimitsExceededException : NexusAPIException
+	{
+		/// <summary>
+		/// The type of the limits that were exceeded.
+		/// </summary>
+		public LimitType LimitType { get; }
+		/// <summary>
+		/// This Exception is thrown whenever a Limit has been exceeded
+		/// </summary>
+		public LimitsExceededException(HttpStatusCode statusCode, LimitType limitType) : base(statusCode)
+		{
+			LimitType = limitType;
+		}
+		/// <summary>
+		/// This Exception is thrown whenever a Limit has been exceeded
+		/// </summary>
+		public LimitsExceededException(string message, HttpStatusCode statusCode, LimitType limitType) : base(message, statusCode)
+		{
+			LimitType = limitType;
+		}
+	}
+}
